e4eb2430a7e69c2fde4b502b07cb17121ae9ff43
Ashwin3290 <ashwinchoubey03092002@gmail.com>
2025-09-16 14:19:50 +0530

    Cleanup wiper.py: Remove commented-out code and streamline deletion of product data

--- wiper.py
+++ wiper.py
@@ -2,15 +2,7 @@ import sqlite3
 
 conn=sqlite3.connect('db.sqlite3')
 c = conn.cursor()
-# c.execute("select * from t_24_Product_Plant_Data_Ext")
-# print(c.fetchall())
-# c.execute("Delete from t_25_Customer_General_Data_mandatory")
-# conn.commit()
-
-# c.execute("""INSERT INTO t_24_Customer_Additional_Addresses (KUNNR, POST_CODE1, CITY1, COUNTRY) SELECT t_24_Customer_General_Data_mandatory.KUNNR, ADRC.POST_CODE1, ADRC.CITY1, ADRC.COUNTRY FROM t_24_Customer_General_Data_mandatory JOIN BUT020 ON t_24_Customer_General_Data_mandatory.KUNNR = BUT020.PARTNER JOIN ADRC ON BUT020.ADDRNUMBER = ADRC.ADDRNUMBER;""")
-# conn.commit()
 
 c.execute("DELETE from t_24_Product_Basic_Data_mandatory_Ext")
-# c.execute("DELETE from t_24_Product_Plant_Data_Ext")
+c.execute("DELETE from t_24_Product_Basic_Data_mandatory_Ext_src")
 conn.commit()
-
